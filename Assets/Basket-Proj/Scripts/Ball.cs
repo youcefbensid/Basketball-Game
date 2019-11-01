80439ea07591a8ae9df339ed4305f2be0f34c2c1
Youcef <afifbensid@hotmail.com>
2019-11-01 21:43:59 +0100

    Full project

---
+++ Assets/Basket-Proj/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject trailParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailParticle.SetActive(false);
+        
+    }
+
+    public void ActivateTrail()
+    {
+        trailParticle.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Goal")
+        {
+            ActivateTrail();
+            GameObject.FindGameObjectsWithTag("arm")[0].GetComponent<Animator>().SetBool("GoDance", true);
+            GameObject.FindGameObjectsWithTag("arm")[1].GetComponent<Animator>().SetBool("GoDance", true);
+            GameObject.FindGameObjectsWithTag("arm2")[0].GetComponent<Animator>().SetBool("GoDance", true);
+            GameObject.FindGameObjectsWithTag("arm2")[1].GetComponent<Animator>().SetBool("GoDance", true);
+
+        }
+    }
+}
---
+++ Assets/Basket-Proj/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Ball ball;
+    public GameObject playerCamera;
+
+    public float ballThrowingForce = 5f;
+    public bool holdingBall = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<AudioSource>().Play(0);
+        }
+        if (holdingBall)
+        {
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * 2;
+            if (Input.GetMouseButtonDown(0))
+            {
+                holdingBall = false;
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward*ballThrowingForce);
+            }
+        }
+        if (Input.GetMouseButtonDown(1) && holdingBall==false)
+        {
+            ball.GetComponent<Rigidbody>().useGravity = false;
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * 2;
+            holdingBall = true;
+            ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            ball.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            ball.trailParticle.SetActive(false);
+        }
+    }
+}
